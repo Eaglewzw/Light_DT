5f0038786962b6709c14c38ca0b62c37d16d2b86
Eaglewzw <1460853569@qq.com>
2024-08-12 12:54:11 +0800

    初始版本

---
+++ 01_Light_DT_code/build/CMakeFiles/Light_DT.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Light_DT.
+# This may be replaced when dependencies are built.
---
+++ 01_Light_DT_code/build/CMakeFiles/uav_detector.dir/CMakeFiles/3.29.0/CompilerIdCUDA/tmp/CMakeCUDACompilerId.cudafe1.cpp.o.d
@@ -0,0 +1,20 @@
+CMakeFiles/uav_detector.dir/CMakeFiles/3.29.0/CompilerIdCUDA/tmp/CMakeCUDACompilerId.cudafe1.cpp.o: \
+ /home/verse/C_plus_plus/Light_DT/01_Light_DT_code/build/CMakeFiles/3.29.0/CompilerIdCUDA/tmp/CMakeCUDACompilerId.cudafe1.cpp \
+ /usr/include/stdc-predef.h /usr/local/cuda/include/crt/host_runtime.h \
+ /usr/local/cuda/include/builtin_types.h \
+ /usr/local/cuda/include/device_types.h \
+ /usr/local/cuda/include/crt/host_defines.h \
+ /usr/local/cuda/include/driver_types.h \
+ /usr/local/cuda/include/vector_types.h \
+ /usr/local/cuda/include/surface_types.h \
+ /usr/local/cuda/include/texture_types.h \
+ /usr/local/cuda/include/crt/storage_class.h \
+ /home/verse/C_plus_plus/Light_DT/01_Light_DT_code/build/CMakeFiles/3.29.0/CompilerIdCUDA/tmp/CMakeCUDACompilerId.cudafe1.stub.c \
+ /usr/local/cuda/include/crt/common_functions.h \
+ /usr/local/cuda/include/crt/math_functions.h \
+ /usr/local/cuda/include/crt/func_macro.h \
+ /usr/local/cuda/include/crt/math_functions.hpp \
+ /usr/local/cuda/include/crt/host_defines.h \
+ /usr/local/cuda/include/math_constants.h \
+ /home/verse/C_plus_plus/Light_DT/01_Light_DT_code/build/CMakeFiles/3.29.0/CompilerIdCUDA/tmp/CMakeCUDACompilerId.fatbin.c \
+ /usr/local/cuda/include/fatbinary_section.h
