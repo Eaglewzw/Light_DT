deee9e1aeca0421b144ee4e10d7224d6e3f9bbcd
Eaglewzw <1460853569@qq.com>
2024-08-12 13:12:58 +0800

    初始版本

---
+++ 01_Light_DT_code/build/CMakeFiles/uav_detector.dir/CMakeFiles/3.29.0/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/uav_detector.dir/CMakeFiles/3.29.0/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
+ /home/verse/C_plus_plus/Light_DT/01_Light_DT_code/build/CMakeFiles/3.29.0/CompilerIdCXX/CMakeCXXCompilerId.cpp \
+ /usr/include/stdc-predef.h
---
+++ 01_Light_DT_code/build/CMakeFiles/uav_detector.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uav_detector.dir/LightTrack.cpp.o"
+  "CMakeFiles/uav_detector.dir/LightTrack.cpp.o.d"
+  "CMakeFiles/uav_detector.dir/main.cpp.o"
+  "CMakeFiles/uav_detector.dir/main.cpp.o.d"
+  "CMakeFiles/uav_detector.dir/src/calibrator.cpp.o"
+  "CMakeFiles/uav_detector.dir/src/calibrator.cpp.o.d"
+  "CMakeFiles/uav_detector.dir/src/preprocess.cu.o"
+  "CMakeFiles/uav_detector.dir/src/preprocess.cu.o.d"
+  "CMakeFiles/uav_detector.dir/src/yololayer.cu.o"
+  "CMakeFiles/uav_detector.dir/src/yololayer.cu.o.d"
+  "uav_detector"
+  "uav_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/uav_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
